016c53142af8176615daef17d76c4541682f9569
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-30 15:36:11 -0400

    iteration 1-3

--- src/App.js
+++ src/App.js
@@ -5,23 +5,86 @@ import contacts from './contacts.json';
 class App extends Component {
 
   state = {
-    showingContacts: contacts.splice(0,5), //This will have the first 5 
-    restOfContacts: contacts  //This will have everyone else excpet the first 5 
+    showingContacts: [...contacts].splice(0,5), //This will have the first 5 
+    restOfContacts: [...contacts].splice(5),  //This will have everyone else except the first 5 
   }
 
   showContacts = () => {
     let contactList = this.state.showingContacts.map((eachContact,i) => {
-      return <li key={i}>{eachContact.name} {eachContact.popularity}</li>
+      return <tr key={eachContact.id}>
+      <td><img style={{width: '50px'}} src={eachContact.pictureUrl} alt={eachContact.name}/></td>
+      <td>{eachContact.name}</td>
+      <td>{eachContact.popularity}</td>
+    </tr>
     })    
     return contactList
   }
 
+  addContact = () => {
+    let randomI = Math.floor(Math.random()*this.state.restOfContacts.length)
+    let showingContactsCopy = [...this.state.showingContacts]
+    let restOfContactsCopy = [...this.state.restOfContacts]
+    showingContactsCopy.push(restOfContactsCopy[randomI])
+    restOfContactsCopy.splice(randomI,1)
+
+    this.setState({
+      showingContacts: showingContactsCopy,
+      restOfContacts: restOfContactsCopy, 
+      newProp: 'Daniel'
+    })
+  }
+
+  sortName = () => {
+    let showingContactsCopy = [...this.state.showingContacts]
+    showingContactsCopy.sort((a,b) => {
+      return a.name.localeCompare(b.name)
+      // if(a.name > b.name){
+      //   return 1000
+      // } else if(a.name < b.name) {
+      //   return -1000
+      // }
+      // return 0
+    })
+    this.setState({
+      showingContacts: showingContactsCopy
+    })
+  }
+
+  sortPop = () => {
+    let showingContactsCopy = [...this.state.showingContacts]
+    showingContactsCopy.sort((a,b) => {
+      return a.popularity - b.popularity
+      // if(a.popularity > b.popularity){
+      //   return 1000
+      // } else if(a.popularity < b.popularity) {
+      //   return -1000
+      // }
+      // return 0
+    })
+    this.setState({
+      showingContacts: showingContactsCopy
+    })
+  }
+
   render() {
+    console.log(this.state.showingContacts.length, this.state.restOfContacts.length)
     return (
       <div>
-
-        {/* { this.state.contacts } */}
-        { this.showContacts() }        
+        <button onClick={this.addContact}>Add Random Contact</button>  
+        <button onClick={this.sortName}>Sort by name</button>  
+        <button onClick={this.sortPop}>Sort by popularity</button>  
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+          { this.showContacts() }
+          </tbody>
+        </table>
       </div>
     );
   }
