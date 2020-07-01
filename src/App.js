5044729bb72f817780122d9d91a6601738101bf6
Cule219 <culaficstefan@gmail.com>
2020-06-30 20:10:41 -0400

    delete

--- src/App.js
+++ src/App.js
@@ -1,89 +1,109 @@
 import React, { Component } from 'react';
 import contacts from './contacts.json';
 
-
 class App extends Component {
-
   state = {
-    showingContacts: [...contacts].splice(0,5), //This will have the first 5 
-    restOfContacts: [...contacts].splice(5),  //This will have everyone else except the first 5 
-  }
+    showingContacts: [...contacts].splice(0, 5), //This will have the first 5
+    restOfContacts: [...contacts].splice(5), //This will have everyone else except the first 5
+  };
 
   showContacts = () => {
-    let contactList = this.state.showingContacts.map((eachContact,i) => {
-      return <tr key={eachContact.id}>
-      <td><img style={{width: '50px'}} src={eachContact.pictureUrl} alt={eachContact.name}/></td>
-      <td>{eachContact.name}</td>
-      <td>{eachContact.popularity}</td>
-    </tr>
-    })    
-    return contactList
-  }
+    let contactList = this.state.showingContacts.map((eachContact, i) => {
+      return (
+        <tr key={eachContact.id}>
+          <td>
+            <img
+              style={{ width: '50px' }}
+              src={eachContact.pictureUrl}
+              alt={eachContact.name}
+            />
+          </td>
+          <td>{eachContact.name}</td>
+          <td>{eachContact.popularity}</td>
+          <td>
+            <button onClick={() => this.deleteRow(i)}>Delete</button>
+          </td>
+        </tr>
+      );
+    });
+    return contactList;
+  };
+
+  deleteRow = (index) => {
+    console.log(index, 'this just got deleted');
+    let showContactsChanged = [...this.state.showingContacts];
+    showContactsChanged.splice(index, 1);
+    this.setState({
+      showingContacts: showContactsChanged,
+    });
+  };
 
   addContact = () => {
-    let randomI = Math.floor(Math.random()*this.state.restOfContacts.length)
-    let showingContactsCopy = [...this.state.showingContacts]
-    let restOfContactsCopy = [...this.state.restOfContacts]
-    showingContactsCopy.push(restOfContactsCopy[randomI])
-    restOfContactsCopy.splice(randomI,1)
+    let randomI = Math.floor(Math.random() * this.state.restOfContacts.length);
+    let showingContactsCopy = [...this.state.showingContacts];
+    let restOfContactsCopy = [...this.state.restOfContacts];
+    showingContactsCopy.push(restOfContactsCopy[randomI]);
+    restOfContactsCopy.splice(randomI, 1);
 
     this.setState({
       showingContacts: showingContactsCopy,
-      restOfContacts: restOfContactsCopy, 
-      newProp: 'Daniel'
-    })
-  }
+      restOfContacts: restOfContactsCopy,
+      newProp: 'Daniel',
+    });
+  };
 
   sortName = () => {
-    let showingContactsCopy = [...this.state.showingContacts]
-    showingContactsCopy.sort((a,b) => {
-      return a.name.localeCompare(b.name)
+    let showingContactsCopy = [...this.state.showingContacts];
+    showingContactsCopy.sort((a, b) => {
+      return a.name.localeCompare(b.name);
       // if(a.name > b.name){
       //   return 1000
       // } else if(a.name < b.name) {
       //   return -1000
       // }
       // return 0
-    })
+    });
     this.setState({
-      showingContacts: showingContactsCopy
-    })
-  }
+      showingContacts: showingContactsCopy,
+    });
+  };
 
   sortPop = () => {
-    let showingContactsCopy = [...this.state.showingContacts]
-    showingContactsCopy.sort((a,b) => {
-      return a.popularity - b.popularity
+    let showingContactsCopy = [...this.state.showingContacts];
+    showingContactsCopy.sort((a, b) => {
+      return a.popularity - b.popularity;
       // if(a.popularity > b.popularity){
       //   return 1000
       // } else if(a.popularity < b.popularity) {
       //   return -1000
       // }
       // return 0
-    })
+    });
     this.setState({
-      showingContacts: showingContactsCopy
-    })
-  }
+      showingContacts: showingContactsCopy,
+    });
+  };
 
   render() {
-    console.log(this.state.showingContacts.length, this.state.restOfContacts.length)
+    console.log(
+      this.state.showingContacts.length,
+      this.state.restOfContacts.length
+    );
     return (
       <div>
-        <button onClick={this.addContact}>Add Random Contact</button>  
-        <button onClick={this.sortName}>Sort by name</button>  
-        <button onClick={this.sortPop}>Sort by popularity</button>  
+        <button onClick={this.addContact}>Add Random Contact</button>
+        <button onClick={this.sortName}>Sort by name</button>
+        <button onClick={this.sortPop}>Sort by popularity</button>
         <table>
           <thead>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
+              <th>Action</th>
             </tr>
           </thead>
-          <tbody>
-          { this.showContacts() }
-          </tbody>
+          <tbody>{this.showContacts()}</tbody>
         </table>
       </div>
     );
